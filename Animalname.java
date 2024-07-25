fa164105f6a0182e990372b3c0a05dd18e6e757c
Renukamathpati <renukam.xworkz@gmail.com>
2024-07-25 14:46:25 +0530

    Add files via upload

---
+++ Animalname.java
@@ -0,0 +1,44 @@
+class Animalname
+{
+	
+	public static void main(String[] args)
+	{
+		System.out.println("start main in animalname");
+		
+		String name="dog";
+		String name1="cat";
+		String name2="rat";
+		String name3="cow";
+		String name4="hourse ";
+		String name5="buffelow";
+		String name6="eliphent ";
+		String name7="zibra";
+		String name8="giraffe ";
+		String name9="lion ";
+		String name10="camel";
+		String name11="tiger";
+		String name12="cheeta";
+		String name13="frog";
+		String name14="gorila";
+		String name15="pig";
+		String name16="sheep";
+		String name17="monkey ";
+		String name18="rabbit ";
+		String name19="kangaroo ";
+		
+		
+		String[] animals={name,name2,name3,name4,name5,name6,name7,name8,name9,name10,name11,name12,name13,name14,name15,name15,name16,name17,name18,name19};
+		
+		for(int index=0;index<animals.length;index++)
+		{
+			String ref=animals[index];
+			System.out.println("animals:"+ref);
+		}
+		System.out.println("total length of animals"+animals.length);
+	System.out.println("end main in animalname");
+	}
+}
+		
+		
+		
+		
