ec8128de606545e92d15f7f9c917a82dff8bc893
Renukamathpati <renukam.xworkz@gmail.com>
2024-07-26 11:53:05 +0530

    Add files via upload

---
+++ Pan.java
@@ -0,0 +1,95 @@
+class Pan
+{
+
+	public Pan()
+{
+	System.out.println("created pan");
+}
+}
+class Water
+{
+
+	public Water()
+	{
+		System.out.println("created water");
+	}
+}
+class Flor
+{
+	public Flor()
+	{
+	System.out.println("created flor");
+	}
+	}
+class Salt
+{
+	public Salt()
+	{
+	System.out.println("created salt");
+}
+}
+class Oil
+{
+	public Oil()
+	{
+	System.out.println("created oil");
+	}	
+	}
+	class Bowl
+	{
+	public Bowl()	
+	{
+	System.out.println("created Bowl");	
+	}
+	}
+class Person
+{
+	public Person()
+	{
+		System.out.println("created person");
+	}		
+	}
+class Hand
+{
+	public Hand()
+
+	{
+		System.out.println("created hand");
+	}
+}
+class Stove
+{
+public Stove()
+{
+System.out.println("created stove");	
+}	
+}
+class Gas
+{
+	public Gas()
+	{
+	System.out.println("created gas");	
+	}
+}
+class Rollerstic
+{
+public Rollerstic()
+
+{
+	System.out.println("created rollerstic");
+}
+}
+class Lighter
+{
+	public Lighter()
+	{
+		System.out.println("created lighter");
+	}
+}
+class Speaker
+{
+	public Speaker()
+	{
+	System.out.println("created speaker");	
+	}
+}
