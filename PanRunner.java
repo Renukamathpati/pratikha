278241b490147d815e32ff57f48d3793472560aa
Renukamathpati <renukam.xworkz@gmail.com>
2024-07-26 11:53:24 +0530

    Add files via upload

---
+++ PanRunner.java
@@ -0,0 +1,20 @@
+class PanRunner
+{
+public static void main(String[] args)
+{
+new Pan();
+new Water();
+new Flor();
+new Salt();
+new Bowl();
+new Person();
+new Hand();
+new Stove();
+new Rollerstic();
+new Lighter();
+new Speaker();
+{
+	System.out.println("its output");
+}
+}
+}
