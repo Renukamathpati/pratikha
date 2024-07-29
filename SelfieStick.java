b0420f141f2215625f903636697ec5813fe2807f
Renukamathpati <renukam.xworkz@gmail.com>
2024-07-29 11:20:26 +0530

    Add files via upload

---
+++ SelfieStick.java
@@ -0,0 +1,223 @@
+
+ class SelfieStick
+ {
+	int selfiestickAmnt;
+	double selfiestickSizeinfeet;
+	
+	
+	public SelfieStick()
+	{
+		
+		System.out.println("created selfiestick");
+	}	
+	
+}
+ class Niriksha
+ {
+	 
+	 int nirikshaWeight;
+	 double nirikshaHight;
+	 
+	 public Niriksha()
+	 {
+		 System.out.println("created niriksha");
+	 }
+ }
+class Wardrobe
+{
+	int wardrobeSize;
+	double wardrobePrice;
+	
+	public Wardrobe()
+	{
+		 System.out.println("created wardrobe");
+	}
+}
+class Key
+{
+	int keySize;
+	double keyPrice;
+	
+	public Key()
+	{
+		 System.out.println("created key");
+	}
+	
+}
+class Atm
+{
+	long atmNum;
+	double atmSize;
+	
+	public Atm()
+	{
+		 System.out.println("created Atm");
+	}
+	
+}
+class Coin
+{
+	double coinSize;
+	String coinShape;
+	
+	public Coin()
+	{
+		 System.out.println("created Coin");
+	}
+	
+}
+class Cloth
+{
+	double clothPrice;
+	String clothFabric;
+	
+	public Cloth()
+	{
+		 System.out.println("created Cloth");
+	}
+	
+}
+class Furniture
+{
+	double furniturePrice;
+	String furnitureShape;
+	
+	public Furniture()
+	{
+		 System.out.println("created Furniture");
+	}
+	
+}
+class Aeroplane
+{
+	double aeroplanePrice;
+	String aeroplaneName;
+	
+	public Aeroplane()
+	{
+		 System.out.println("created Aeroplane");
+	}
+	
+}
+
+class Cctv
+{
+	double cctvPrice;
+	String cctvName;
+	
+	public Cctv()
+	{
+		 System.out.println("created Cctv");
+	}
+	
+}
+
+class Tripode
+{
+	double tripodePrice;
+	double tripodeSizeinfeet;
+	
+	public Tripode()
+	{
+		 System.out.println("created Tripode");
+	}
+	
+}
+
+class Zip
+{
+	double zipPrice;
+	double zipSizeinfeet;
+	
+	public Zip()
+	{
+		 System.out.println("created Zip");
+	}
+	
+}
+
+
+class Helmete
+{
+	double helmetePrice;
+	String shapeofHelmete;
+	
+	public Helmete()
+	{
+		 System.out.println("created Helmete");
+	}
+}
+	
+class Hillstation
+{
+	String hillstationName;
+	String hillstationLocation;
+	
+	public Hillstation()
+	{
+		 System.out.println("created Hillstation");
+	}
+	
+}
+	
+	
+	
+
+class Hat
+{
+	double hatPrice;
+	String shapeofHat;
+	
+	public Hat()
+	{
+		 System.out.println("created Hat");
+	}
+	}
+
+	
+	class Camera
+{
+	double cameraPrice;
+	String cameraName;
+	
+	public Camera()
+	{
+		 System.out.println("created Camera");
+	}
+	}
+
+class Poster
+{
+	double posterPrice;
+	String posterName;
+	
+	public Poster()
+	{
+		 System.out.println("created Poster");
+	}
+	}
+	
+	class Postbox
+{
+	double postboxSizeinfeet;
+	String postboxName;
+	
+	public Postbox()
+	{
+		 System.out.println("created Postbox");
+	}
+	}
+	
+	class Tree
+{
+	String treeName;
+	String treeShape;
+	
+	public Tree()
+	{
+		 System.out.println("created Tree");
+	}
+	}
+
+
+
