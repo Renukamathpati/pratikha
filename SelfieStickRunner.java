0c1f2a7eb885eead30185ebb19ab7d88794709b3
Renukamathpati <renukam.xworkz@gmail.com>
2024-07-29 11:19:50 +0530

    Add files via upload

---
+++ SelfieStickRunner.java
@@ -0,0 +1,252 @@
+class SelfieStickRunner
+{
+	public static void main(String[] poornima)
+	{
+		System.out.println("running main in selfiestick runner");
+		SelfieStick selfiestick=new SelfieStick();
+		int amntofSelfiestick=selfiestick.selfiestickAmnt;
+		double sizeofSelfiestick=selfiestick.selfiestickSizeinfeet;
+		
+		System.out.println("default-values of amntofSelfiestick:"+amntofSelfiestick);
+		System.out.println("default-values of sizeofSelfiestick:"+sizeofSelfiestick);
+		
+		amntofSelfiestick=450;
+		sizeofSelfiestick=80.55;
+		System.out.println("updated-values:"+amntofSelfiestick);
+		System.out.println("updated-values:"+sizeofSelfiestick);
+		
+		System.out.println("ending main in selfiestick runner");
+		
+	
+		Niriksha niriksha=new Niriksha();
+		int weightofNiriksha=niriksha.nirikshaWeight;
+		double hightofNiriksha=niriksha.nirikshaHight;
+		
+		System.out.println("default-values of weightofNiriksha:"+weightofNiriksha);
+		System.out.println("default-values of hightofNirikshahight:"+hightofNiriksha);
+		
+		weightofNiriksha=45;
+		hightofNiriksha=5.2;
+		System.out.println("updated-values:"+weightofNiriksha);
+		System.out.println("updated-values:"+hightofNiriksha);
+		
+		Wardrobe wardrobe=new Wardrobe();
+		int sizeofWardrobe=wardrobe.wardrobeSize;
+		double priceofwardrobe=wardrobe.wardrobePrice;
+		
+		System.out.println("default-values of sizeofWardrobe:"+sizeofWardrobe);
+		System.out.println("default-values of priceofwardrobe:"+sizeofWardrobe);
+		
+		sizeofWardrobe=5;
+		priceofwardrobe=5000;
+		System.out.println("updated-values:"+sizeofWardrobe);
+		System.out.println("updated-values:"+priceofwardrobe);
+		
+		Key key=new Key();
+		int sizeofKey=key.keySize;
+		double priceofKey=key.keyPrice;
+		
+		System.out.println("default-values of sizeofKey:"+sizeofKey);
+		System.out.println("default-values of priceofKey:"+priceofKey);
+		
+		sizeofKey=5;
+		priceofKey=50;
+		System.out.println("updated-values:"+sizeofKey);
+		System.out.println("updated-values:"+priceofKey);
+		
+		Atm atm=new Atm();
+		long numofAtm=atm.atmNum;
+		double sizeofAtm=atm.atmSize;
+		
+		System.out.println("default-values of numofAtm:"+numofAtm);
+		System.out.println("default-values of sizeofAtm:"+sizeofAtm);
+		
+		numofAtm=223443122l;
+		sizeofAtm=3.1;
+		System.out.println("updated-values:"+numofAtm);
+		System.out.println("updated-values:"+sizeofAtm);
+		
+		Coin coin=new Coin();
+		double sizeofCoin=coin.coinSize;
+		String shapeofCoin=coin.coinShape;
+		
+		System.out.println("default-values of sizeofCoin :"+sizeofCoin);
+		System.out.println("default-values of shapeofCoin:"+shapeofCoin);
+		
+		sizeofCoin=2.1;
+		shapeofCoin="round";
+		System.out.println("updated-values:"+sizeofCoin);
+		System.out.println("updated-values:"+shapeofCoin);
+		
+		
+		Cloth cloth=new Cloth();
+		double priceofCloth=cloth.clothPrice;
+		String fabricofCloth=cloth.clothFabric;
+		
+		System.out.println("default-values of priceofCloth :"+priceofCloth);
+		System.out.println("default-values of fabricofCloth:"+fabricofCloth);
+		
+		priceofCloth=2000;
+		fabricofCloth="silk";
+		System.out.println("updated-values:"+priceofCloth);
+		System.out.println("updated-values:"+fabricofCloth);
+		
+		
+		
+		Furniture furniture=new Furniture();
+		double priceofFurniture=furniture.furniturePrice;
+		String shapeofFurniture=furniture.furnitureShape;
+		
+		System.out.println("default-values of priceofFurniture:"+priceofFurniture);
+		System.out.println("default-values of shapeofFurniture:"+shapeofFurniture);
+		
+		priceofFurniture=2000;
+		shapeofFurniture="circle";
+		System.out.println("updated-values:"+priceofFurniture);
+		System.out.println("updated-values:"+shapeofFurniture);
+		
+		
+		Aeroplane aeroplane=new Aeroplane();
+		double priceofAeroplane=aeroplane.aeroplanePrice;
+		String nameofAeroplane=aeroplane.aeroplaneName;
+		
+	System.out.println("default-values of priceofAeroplane:"+priceofAeroplane);
+	System.out.println("default-values of nameofAeroplane:"+nameofAeroplane);
+		
+		priceofAeroplane=20000;
+		nameofAeroplane="indigo";
+		System.out.println("updated-values:"+priceofAeroplane);
+		System.out.println("updated-values:"+nameofAeroplane);
+		
+		
+		Cctv cctv=new Cctv();
+		double priceofCctv=cctv.cctvPrice;
+		String nameofCctv=cctv.cctvName;
+		
+	System.out.println("default-values of priceofCctv:"+priceofCctv);
+	System.out.println("default-values of nameofCctv:"+nameofCctv);
+		
+		priceofCctv=20000;
+		nameofCctv="sony";
+		System.out.println("updated-values:"+priceofCctv);
+		System.out.println("updated-values:"+nameofCctv);
+		
+		Tripode tripode=new Tripode();
+		double priceofTripode=tripode.tripodePrice;
+		double sizeofTripode=tripode.tripodeSizeinfeet;
+		
+	System.out.println("default-values of priceofTripode:"+priceofTripode);
+	System.out.println("default-values of sizeofTripode:"+sizeofTripode);
+		
+		priceofTripode=20000;
+		sizeofTripode=2.5;
+		System.out.println("updated-values:"+priceofTripode);
+		System.out.println("updated-values:"+sizeofTripode);
+		
+		
+		
+		Zip zip=new Zip();
+		double priceofZip=zip.zipPrice;
+		double sizeofZip=zip.zipSizeinfeet;
+		
+	System.out.println("default-values of priceofZip:"+priceofZip);
+	System.out.println("default-values of sizeofZip:"+sizeofZip);
+		
+		priceofZip=20;
+		sizeofZip=1.0;
+		System.out.println("updated-values:"+priceofZip);
+		System.out.println("updated-values:"+sizeofZip);
+		
+		
+		Helmete helmete=new Helmete();
+		double priceofHelmete=helmete.helmetePrice;
+		String shapeofHelmete=helmete.shapeofHelmete;
+		
+	System.out.println("default-values of priceofHelmete:"+priceofHelmete);
+	System.out.println("default-values of shapeofHelmete:"+shapeofHelmete);
+		
+		priceofHelmete=500;
+		shapeofHelmete="round";
+		System.out.println("updated-values:"+priceofHelmete);
+		System.out.println("updated-values:"+shapeofHelmete);
+		
+		
+		
+		Hillstation hillstation=new Hillstation();
+		String nameofHillstation=hillstation.hillstationName;
+		String locationofHillstation=hillstation.hillstationLocation;
+		
+	System.out.println("default-values of nameofHillstation:"+nameofHillstation);
+	System.out.println("default-values of locationofHillstation:"+locationofHillstation);
+		
+		nameofHillstation="nandihills";
+		locationofHillstation="chikkballapur";
+		System.out.println("updated-values:"+nameofHillstation);
+		System.out.println("updated-values:"+locationofHillstation);
+		
+		
+		
+		Hat hat=new Hat();
+		double priceofHat=hat.hatPrice;
+		String shapeofHat=hat.shapeofHat;
+		
+	System.out.println("default-values of priceofHat:"+priceofHat);
+	System.out.println("default-values of shapeofHat:"+shapeofHat);
+		
+		priceofHat=500;
+		shapeofHat="round";
+		System.out.println("updated-values:"+priceofHat);
+		System.out.println("updated-values:"+shapeofHat);
+		
+		Camera camera=new Camera();
+		double priceofCamera=camera.cameraPrice;
+		String nameofCamera=camera.cameraName;
+		
+	System.out.println("default-values of priceofCamera:"+priceofCamera);
+	System.out.println("default-values of nameofCamera:"+nameofCamera);
+		
+		priceofCamera=5000;
+		nameofCamera="sony";
+		System.out.println("updated-values:"+priceofCamera);
+		System.out.println("updated-values:"+nameofCamera);
+		
+		
+		Poster poster=new Poster();
+		double priceofPoster=poster.posterPrice;
+		String nameofPoster=poster.posterName;
+		
+	System.out.println("default-values of priceofPoster:"+priceofPoster);
+	System.out.println("default-values of nameofPoster:"+nameofPoster);
+		
+		priceofPoster=50;
+		nameofPoster="filmposter";
+		System.out.println("updated-values:"+priceofPoster);
+		System.out.println("updated-values:"+nameofPoster);
+		
+		
+		Postbox postbox=new Postbox();
+		double sizeofPostbox=postbox.postboxSizeinfeet;
+		String nameofPostbox=postbox.postboxName;
+		
+	System.out.println("default-values of sizeofPostbox:"+sizeofPostbox);
+	System.out.println("default-values of nameofPostbox:"+nameofPostbox);
+		
+		sizeofPostbox=2.0;
+		nameofPostbox="redbox";
+		System.out.println("updated-values:"+sizeofPostbox);
+		System.out.println("updated-values:"+nameofPostbox);
+		
+		
+		Tree tree=new Tree();
+		String nameofTree=tree.treeName;
+		String shapeofTree=tree.treeShape;
+		
+	System.out.println("default-values of nameofTree:"+nameofTree);
+	System.out.println("default-values of shapeofTree:"+shapeofTree);
+		
+		nameofTree="mango";
+		shapeofTree="round";
+		System.out.println("updated-values:"+nameofTree);
+		System.out.println("updated-values:"+shapeofTree);
+	}
+}
